4977a7deaa95975fe1296fecd23521f3fef95c26
Luke Chinworth <luke.chinworth@soliddigital.com>
2018-01-24 13:43:32 -0800

    catch error if Vue fails

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
-﻿new Vue({
-    el: '#app',
-    render(h) {
-        return h('div', [
-            h('h1', 'Vue app'),
-            h('button', 'Button 1'),
-            h('button', 'Button 2'),
-            h('button', {
-                style: {
-                    display: 'block'
-                }
-            }, 'Button 3')
-        ]);
-    }
-});
+﻿try {
+    new Vue({
+        el: '#app',
+        render(h) {
+            return h('div', [
+                h('h1', 'Vue app'),
+                h('button', 'Button 1'),
+                h('button', 'Button 2'),
+                h('button', {
+                    style: {
+                        display: 'block'
+                    }
+                }, 'Button 3')
+            ]);
+        }
+    });
+} catch(e) {
+    var p = document.createElement('p');
+    p.innerText = e.message;
+
+    document.body.appendChild(p)
+}

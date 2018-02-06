0ad91c90106f62169e550c88280c53053e743d6f
Luke Chinworth <luke.chinworth@soliddigital.com>
2018-02-06 09:40:17 -0800

    remove hanging comma

--- js/main.js
+++ js/main.js
@@ -29,46 +29,33 @@ try {
     var LA_URL = 'https://cwip-shaka-proxy.appspot.com/no_auth';
     var DASH_MANIFEST = 'https://demo.unified-streaming.com/video/tears-of-steel/tears-of-steel-dash-widevine.ism/.mpd';
 
-    document.addEventListener('DOMContentLoaded', initApp);
 
-    function initApp() {
-        // Install built-in polyfills to patch browser incompatibilities.
-        shaka.polyfill.installAll();
+    shaka.polyfill.installAll();
 
-        // Check to see if the browser supports the basic APIs Shaka needs.
-        if (shaka.Player.isBrowserSupported()) {
-            // Everything looks good!
-            initPlayer();
-        } else {
-            // This browser does not have the minimum set of APIs we need.
-            console.error('Browser not supported!');
-        }
+    if (!shaka.Player.isBrowserSupported()) {
+        throw new Error('shaka unable to polyfill the browser');
     }
 
-    function initPlayer() {
-        // Create a Player instance.
-        var video = document.getElementById('drm');
-        var player = new shaka.Player(video);
+    var video = document.getElementById('drm');
+    var player = new shaka.Player(video);
 
-        player.addEventListener('error', function(event) {
-            appendError(event.detail);
-        });
+    player.addEventListener('error', function(event) {
+        appendError(event.detail);
+    });
 
-        // set widevine drm
-        player.configure({
-            drm: {
-                servers: {
-                    'com.widevine.alpha': LA_URL,
-                }
+    // set widevine drm
+    player.configure({
+        drm: {
+            servers: {
+                'com.widevine.alpha': LA_URL
             }
-        });
+        }
+    });
 
-        player.load(DASH_MANIFEST)
-            .then(function() {
-                console.log('The video has now been loaded!');
-            })
-            .catch(appendError);
-    }
+    player.load(DASH_MANIFEST)
+        .then(function() {
+            console.log('The video has now been loaded!');
+        })
 } catch(e) {
     appendError(e);
 }
